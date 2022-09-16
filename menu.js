0d980e3207cc875b18659d7df214c8f9214c9fec
Red Monzon <redkmonzon30@gmail.com>
2022-09-16 15:24:26 -0500

    Validate the menu.html

---
+++ menu.js
@@ -0,0 +1,21 @@
+const playButton = document.getElementById('play-button');
+const username = document.querySelector('#username');
+const form = document.getElementById('submission-form');
+const error = document.getElementById('error');
+
+username.addEventListener('keyup', () => {
+    username.style.backgroundColor = "red";
+
+    if(username.value === '' || username.value == null){
+        username.style.backgroundColor = "white";
+    }
+})
+
+form.addEventListener('submit', (e) =>{
+    if(username.value === '' || username.value == null){
+        error.style.display = 'block';
+        username.setAttribute('style', 'border-color: red; border-width: 3px');
+        e.preventDefault();
+    }   
+})
+
