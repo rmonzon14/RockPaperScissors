ec84be932f180f583fe09a08d849ddf99ed36e50
Red Monzon <redkmonzon30@gmail.com>
2022-09-27 10:57:18 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,71 @@
-const user = document.getElementsByClassName('user')[0];
+const user = document.getElementById('user');
+const redScore = document.getElementById('red-score');
+const userScore = document.getElementById('user-score');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const roundInfo = document.getElementById('round-info');
+const redPicked = document.getElementById('red-picked');
+const roundResult = document.getElementById('round-result');
 
+// Sets the username of the user. 
 user.innerHTML = localStorage.getItem('player');
+
+function getRedChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const choicePicker = Math.floor(Math.random() * 3);
+    return choices[choicePicker];
+}
+
+function updateScore(player) {
+    player.innerHTML = parseInt(player.innerHTML) + 1;    
+}
+
+function displayRoundInfo(redChoice, result) {
+    roundInfo.style.display = 'block';
+    redPicked.textContent = redChoice;
+    roundResult.textContent = result;
+}
+
+function getResult(userChoice) {
+    const redChoice = getRedChoice();
+    const checkResult =  redChoice + userChoice;
+    switch (checkResult) {
+        case 'scissorspaper':
+        case 'paperrock':
+        case 'rockscissors':
+            console.log("you lose")
+            updateScore(redScore)
+            displayRoundInfo(redChoice, 'You lost!')
+            break;
+        case 'paperscissors':
+        case 'rockpaper':
+        case 'scissorsrock':
+            updateScore(userScore)
+            displayRoundInfo(redChoice, 'You win!')
+            break;
+        case 'paperpaper':
+        case 'rockrock':
+        case 'scissorsscissors':
+            displayRoundInfo(redChoice, 'It\'s a draw')
+            break;
+    }
+}
+
+function playRound() {
+    rock.addEventListener('click', ()  => {
+        getResult('rock');
+    })
+    paper.addEventListener('click', () => {
+        getResult('paper');
+    })
+    scissors.addEventListener('click', () => {
+        getResult('scissors');
+    })
+}
+
+playRound();
+
+
+
+
